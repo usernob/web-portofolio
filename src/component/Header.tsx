2c592d9e142c9375f7d531675abf54ec27c527a7
usernob <shamsulmaarif2016@gmail.com>
2022-10-10 00:25:43 +0700

    add some update

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 export default function Header() {
     const [open, setOpen] = useState(false);
     const hamburgerClassList = [
-        ["origin-top-left", "rotate-45"],
-        ["", "-translate-x-full scale-0"],
-        ["origin-bottom-left", "-rotate-45"],
+        ["origin-right", "-rotate-45"],
+        ["", "translate-x-full scale-0"],
+        ["origin-right", "rotate-45"],
     ];
 
     const hamburger = hamburgerClassList.map(
         ([classBefore, classAfter], index) => {
-            let classes = `my-2 block h-[2px] w-[29px] bg-slate-900 transition duration-300 ease-in-out dark:bg-slate-100 ${classBefore} ${
+            let classes = `my-2 block h-[2px] w-[29px] transition duration-300 ease-in-out bg-slate-100 mx-4 bg-gradient-to-r from-blue-500 to-teal-500 bg-clip-content ${classBefore} ${
                 open ? classAfter : ""
             }`;
             return <span key={index} className={classes}></span>;
@@ -23,10 +23,13 @@ export default function Header() {
     ];
     const navMenu = navData.map(([content, path], index) => {
         return (
-            <li key={index} className="group">
+            <li
+                key={index}
+                className="group bg-gradient-to-r from-blue-500 to-teal-500 bg-clip-text"
+            >
                 <a
                     href={path}
-                    className="mx-8 flex py-2 text-base text-slate-200 group-hover:text-blue-500"
+                    className="mx-8 flex py-2 text-base text-transparent group-hover:text-blue-500"
                 >
                     {content}
                 </a>
@@ -38,15 +41,15 @@ export default function Header() {
         "text-base text-slate-200 py-2 mx-8 flex group-hover:text-teal-500";
     let classMenu = `${
         open ? navItemsClass : "hidden"
-    } absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-slate-700 py-2 shadow-lg xl:static xl:block xl:max-w-full xl:rounded-none xl:bg-transparent xl:shadow-none`;
+    } absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-slate-800 py-2 shadow-lg xl:static xl:block xl:max-w-full xl:rounded-none xl:bg-transparent xl:shadow-none`;
 
     return (
         <header
-            className="fixed top-0 left-0 z-20 flex w-full items-center bg-slate-900 bg-opacity-90 backdrop-blur-sm"
+            className="sticky top-0 left-0 z-20 flex w-full items-center bg-slate-900 bg-opacity-90 backdrop-blur-sm lg:fixed"
             id="header"
         >
             <div className="w-full md:container">
-                <div className="flex items-center justify-between">
+                <div className="relative flex items-center justify-between">
                     <div className="px-4">
                         <a
                             href="#"
@@ -56,14 +59,14 @@ export default function Header() {
                             ~$ <span className="wrap">USERNOB</span>
                         </a>
                     </div>
-                    <div className="flex items-center px-4">
-                        <button
+                    <div className="flex items-center xl:bg-gradient-to-r xl:from-blue-500 xl:to-teal-500 xl:bg-clip-text">
+                        <div
                             id="hamburger"
                             className="block xl:hidden"
                             onClick={() => setOpen(!open)}
                         >
                             {hamburger}
-                        </button>
+                        </div>
                         <nav id="nav-menu" className={classMenu}>
                             <ul className="block xl:flex">{navMenu}</ul>
                         </nav>
--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -1,18 +1,18 @@
 export default function Hero() {
     return (
         <section className="mb-96 h-[100vh] gap-2 lg:flex lg:flex-row-reverse lg:justify-end">
-            <div className="hidden h-[20rem] flex-1 items-center justify-center pt-16 xs:flex lg:h-full lg:py-20">
-                <h1 className="absolute inline-block -translate-x-3 translate-y-6 skew-x-[16deg] text-xs text-[13rem] font-bold text-transparent opacity-30 [-webkit-text-stroke:2px_#d946ef] sm:text-[17rem]">
+            <div className="hidden h-[20rem] flex-1 items-center justify-center xs:flex lg:h-full">
+                <h1 className="absolute inline-block -translate-x-2 translate-y-4 skew-x-[16deg] text-xs text-[13rem] font-bold text-transparent opacity-30 [-webkit-text-stroke:2px_#d946ef] sm:text-[17rem]">
                     R
                 </h1>
                 <h1 className="absolute inline-block skew-x-[16deg] text-xs text-[13rem] font-bold text-transparent opacity-60 [-webkit-text-stroke:2px_#2563eb] sm:text-[17rem]">
                     R
                 </h1>
-                <h1 className="absolute inline-block translate-x-3 -translate-y-6 skew-x-[16deg] text-xs text-[13rem] font-bold text-slate-300 sm:text-[17rem]">
+                <h1 className="absolute inline-block translate-x-2 -translate-y-4 skew-x-[16deg] text-xs text-[13rem] font-bold text-slate-300 sm:text-[17rem]">
                     R
                 </h1>
             </div>
-            <div className="flex flex-1 flex-col justify-center gap-3 pt-16 xs:py-4 lg:py-20">
+            <div className="flex flex-1 flex-col justify-center gap-3 xs:py-4">
                 <h2 className="text-2xl font-semibold text-slate-300">
                     Hey there! I am
                 </h2>
