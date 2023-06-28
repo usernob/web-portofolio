dc65af4b007b9649646692f7627385efa2a2fa6d
usernob <shamsulmaarif2016@gmail.com>
2023-06-28 16:56:02 +0700

    fix ScrollAnimation maybe i should call it fade in scroll

--- src/lib/AnimateScroll.tsx
+++ src/lib/AnimateScroll.tsx
@@ -1,99 +1,56 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import throttle from "lodash.throttle";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
-    className?: string;
-    addClass?: string;
-    removeClass?: string;
-    children?: any;
-};
-const AnimateScroll = ({
-    className: classNameProps,
-    addClass,
-    removeClass,
-    children,
-}: Props) => {
-    const [newClass, setNewClass] = useState("");
-    const [classes, setClasses] = useState(classNameProps);
-    const node: { current: any } = useRef(null);
-
-    const getElementTop = useCallback((elm: any) => {
-        let yPos = 0;
-        while (
-            elm &&
-            elm.offsetTop !== undefined &&
-            elm.clientTop !== undefined
-        ) {
-            yPos += elm.offsetTop + elm.clientTop;
-            elm = elm.offsetParent;
-        }
-        return yPos;
-    }, []);
-
-    const getScrollPos = useCallback(() => {
-        if (window.pageYOffset !== undefined) {
-            return window.pageYOffset;
-        }
-        return 0;
-    }, []);
-
-    const getScrollableParentHeight = useCallback(() => {
-        if (window.innerHeight !== undefined) {
-            return window.innerHeight;
-        }
-        return 0;
-    }, []);
-
-    const getViewportTop = useCallback(() => {
-        return getScrollPos();
-    }, [getScrollPos]);
-
-    const getViewportBottom = useCallback(() => {
-        return getScrollPos() + getScrollableParentHeight() - 150;
-    }, [getScrollPos, getScrollableParentHeight]);
-
-    const isBellowViewport = useCallback(() => {
-        const elTop = getElementTop(node.current) - getElementTop(window);
-        return elTop > getViewportBottom();
-    }, [getElementTop, getViewportTop, node]);
-
-    const handleScroll = useCallback(() => {
-        if (!isBellowViewport()) {
-            setNewClass(addClass || "");
-            let pattern = new RegExp(removeClass?.replace(" ", "|") || "", "gm")
-            setClasses(classes?.replace(pattern, ""))
-        } else {
-            setNewClass("");
-            setClasses(classNameProps);
-        }
-    }, [isBellowViewport]);
-
-    const listener = useMemo(
-        () =>
-            throttle(() => {
-                if (node.current) handleScroll();
-            }, 50),
-        [handleScroll]
-    );
-
-    useEffect(() => {
-        if (window) {
-            window.addEventListener("scroll", listener);
-        }
-        return () => {
-            if (window && window.removeEventListener) {
-                window.removeEventListener("scroll", listener);
-            }
-        };
-    }, [handleScroll, listener]);
-    return (
-        <div
-            ref={node}
-            className={classes ? `${classes} ${newClass}` : newClass}
-        >
-            {children}
-        </div>
-    );
+  children: React.ReactNode;
+  className?: string;
+  offset?: number;
+  end?: number;
 };
 
-export default AnimateScroll;
+export default function AnimateScroll(
+  { children, className, offset = 0, end = 200 }: Props,
+): JSX.Element {
+  const node = useRef<HTMLDivElement>(null);
+  const [opacityValue, setOpacityValue] = useState(1);
+  const listener = () => {
+    let rect: DOMRect = node.current !== null
+      ? node.current.getBoundingClientRect()
+      : new DOMRect(0, 0, 1080, 962);
+
+    let viewportStartY: number = window.innerHeight + offset;
+    let viewportEndY: number = end + offset;
+    let distanceFromStart: number = viewportStartY - rect.top;
+    let isInViewPort: boolean = (rect.top < viewportStartY) &&
+      (rect.top > viewportEndY);
+
+    let percentage: number = isInViewPort
+      ? Number.parseFloat(
+        (distanceFromStart / (viewportStartY - viewportEndY)).toFixed(2),
+      )
+      : 1;
+
+    setOpacityValue(percentage);
+
+    if (rect.top < (viewportEndY - 400)) {
+      if (window && window.removeEventListener) {
+        window.removeEventListener("scroll", listener);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (window) {
+      window.addEventListener("scroll", listener);
+    }
+    return () => {
+      if (window && window.removeEventListener) {
+        window.removeEventListener("scroll", listener);
+      }
+    };
+  }, []);
+  return (
+    <div ref={node} className={className} style={{ opacity: opacityValue }}>
+      {children}
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,50 @@ import * as Comp from "./component";
 import AnimateScroll from "./lib/AnimateScroll";
 
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
-    <React.StrictMode>
-        <Comp.Header />
-        <main className="mx-6 md:container pb-96 overflow-x-hidden">
-            <Section.Hero />
-            <Section.About />
-            <AnimateScroll className="w-100 text-9xl text-slate-100 opacity-0 duration-300 ease-in bg-blue-600 translate-x-full delay-100" removeClass="opacity-0 translate-x-full">hi</AnimateScroll>
-            <AnimateScroll className="w-100 text-9xl text-slate-100 opacity-0 duration-300 ease-in bg-blue-600 -translate-x-full delay-100" removeClass="opacity-0 -translate-x-full">hi</AnimateScroll>
-            <AnimateScroll className="w-100 text-9xl text-slate-100 opacity-0 duration-300 ease-in bg-blue-600 translate-x-full delay-100" removeClass="opacity-0 translate-x-full">hi</AnimateScroll>
-            <AnimateScroll className="w-100 text-9xl text-slate-100 opacity-0 duration-300 ease-in bg-blue-600 -translate-x-full delay-100" removeClass="opacity-0 -translate-x-full">hi</AnimateScroll>
-            <AnimateScroll className="w-100 text-9xl text-slate-100 opacity-0 duration-300 ease-in bg-blue-600 translate-x-full delay-100" removeClass="opacity-0 translate-x-full">hi</AnimateScroll>
-            <AnimateScroll className="w-100 text-9xl text-slate-100 opacity-0 duration-300 ease-in bg-blue-600 -translate-x-full delay-100" removeClass="opacity-0 -translate-x-full">hi</AnimateScroll>
-            <AnimateScroll className="w-100 text-9xl text-slate-100 opacity-0 duration-300 ease-in bg-blue-600 translate-x-full delay-100" removeClass="opacity-0 translate-x-full">hi</AnimateScroll>
-        </main>
-        <Comp.Footer />
-        <div onClick={() => {
-            document.getElementById("home")?.scrollIntoView(true)
-        }} className="fixed bottom-5 right-5 bg-blue-500 rounded-full p-4"></div>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Comp.Header />
+    <main className="mx-6 md:container pb-96 overflow-x-hidden">
+      <Section.Hero />
+      <Section.About />
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <div className="my-4">
+        <AnimateScroll className="bg-slate-800 m-4 rounded-lg shadow-lg border border-slate-200 p-4" offset={100}>hi</AnimateScroll>
+      </div>
+      <Section.About />
+    </main>
+    <Comp.Footer />
+    <div
+      onClick={() => {
+        document.getElementById("home")?.scrollIntoView(true);
+      }}
+      className="fixed bottom-5 right-5 bg-blue-500 rounded-full p-4"
+    >
+    </div>
+  </React.StrictMode>,
 );
